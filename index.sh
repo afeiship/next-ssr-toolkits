c46b172e54e056bd30ba3b3e06276cbc077d9d3a
afeiship <1290657123@qq.com>
2022-08-15 11:17:40 +0800

    fix: use-yarn to create react app

--- index.sh
+++ index.sh
@@ -4,8 +4,10 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias cra-redux='npx create-react-app . --use-npm --template redux';
 alias cra-redux-ts='npx create-react-app . --use-npm --template redux-typescript';
-alias cra-js='npx create-react-app . --use-npm';
-alias cra-ts='npx create-react-app . --use-npm --template typescript';
+alias cra-js='yarn create react-app .';
+alias cra-ts='yarn create react-app . --template typescript';
+alias cra-jsn='npx create-react-app . --use-npm';
+alias cra-tsn='npx create-react-app . --use-npm --template typescript';
 
 # add template react-script4.0.4
 alias cra-t4='create-react-app . --template @jswork/typescript4';
